46f3e3060e41e234add3bab93c5a4dfbf6d83e6b
Martmists <martmists@gmail.com>
2022-12-21 15:35:23 +0100

    Properly configure Compose Desktop
    Signed-off-by: Martmists <martmists@gmail.com>

--- mewsic-application/build.gradle.kts
+++ mewsic-application/build.gradle.kts
@@ -1,3 +1,28 @@
 plugins {
     id("mewsic-application")
 }
+
+compose.desktop {
+    application {
+        mainClass = "org.mewsic.application.MainKt"
+
+        nativeDistributions {
+            packageName = "Mewsic"
+            description = "Cross-platform Music Player"
+            packageVersion = (project.version as String).substringAfter('v').substringBefore('-')
+            copyright = "© 2022 Mewsic Developers. All rights reserved."
+            modules("java.instrument", "jdk.unsupported")
+
+            windows {
+                perUserInstall = true
+                dirChooser = true
+            }
+            linux {
+
+            }
+            macOS {
+                bundleID = group as String
+            }
+        }
+    }
+}
